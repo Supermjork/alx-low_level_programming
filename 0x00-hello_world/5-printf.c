7e04eb5c1c5156ab996d0e90f91b29995c49f825
Supermjork <markehabaziz@gmail.com>
2023-07-13 21:06:38 +0300

    BYTES PLS

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: CONTINUATION ONG
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 	/* BOI DO C98 COMMENTS HIT DIFF */
 	char OutStr[] = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", OutStr);
-	return 0;
+	return (0);
 }
