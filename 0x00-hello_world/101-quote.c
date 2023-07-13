4b79f4afdfa0623cef0d528e20cbfaacc7c2f022
Supermjork <markehabaziz@gmail.com>
2023-07-13 21:25:20 +0300

    lil thin stuff forgotten, dw

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - PRINTING REST OF QUOTE LESGOOOOO
  *
- * RETURN: 1 Always (Success)
+ * Return: 1 Always (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
