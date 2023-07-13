8f87f7dde08cbf2d607935234bdafcaaac82becd
Supermjork <markehabaziz@gmail.com>
2023-07-13 21:23:09 +0300

    Bonus?

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+
+/**
+ * main - PRINTING REST OF QUOTE LESGOOOOO
+ *
+ * RETURN: 1 Always (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
