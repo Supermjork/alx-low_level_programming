696888f144eb9b294ee09450ec6a294f9aa24e3a
Supermjork <markehabaziz@gmail.com>
2023-07-14 23:28:59 +0300

    First time?

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - print NUMBAS BIG MAN
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for(num = 0; num < 10; num++)
+		printf("%d", num);
+	
+	return (0);
+}
