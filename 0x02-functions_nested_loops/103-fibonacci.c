156827042819b2bfe602db5128f27b368e20b76f
Supermjork <markehabaziz@gmail.com>
2023-07-18 20:59:10 +0300

    No more, 16 pidor

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - printing fibo
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long n0, n1, fib, sum;
+
+	n0 = 1;
+	n1 = 2;
+	fib = 3;
+	sum = 0;
+
+	while (fib < 4000000)
+	{
+		fib = n0 + n1;
+
+		n0 = n1;
+		n1 = fib;
+
+		if (fib % 2 == 0)
+			sum += fib;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
+
