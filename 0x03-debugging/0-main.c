a5db3cb5ca6ad376e82081351fd7eb79d79a2768
Supermjork <markehabaziz@gmail.com>
2023-07-19 22:12:06 +0300

    Yes

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tests if the function does its thing
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,3 @@
+#include<unistd.h>
+void positive_or_negative(int);
+
