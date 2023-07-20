a326d72159db62a7890473923b9e0e77589ce467
Supermjork <markehabaziz@gmail.com>
2023-07-20 16:55:45 +0300

    we pogging frfr

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 passed numbers
+ *
+ * @num0: first number
+ * @num1: second number
+ *
+ * Return: product of multiplication
+ */
+
+int mul(int num0, int num1)
+{
+	return (num0 * num1);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,5 @@
 #include <unistd.h>
 int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
