1fb781aa026b0474e8fd934fb9ff628196a8ee75
Supermjork <markehabaziz@gmail.com>
2023-07-20 19:05:05 +0300

    suppopu

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle with specified height
+ *
+ * @size: height
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		int height, whites;
+
+		for (height = 0; height < size; height++)
+		{
+			for (whites = 0; whites < size - height; whites++)
+			{
+				_putchar(32);
+			}
+			_putchar(35);
+			_putchar(10);
+		}
+	}
+}
