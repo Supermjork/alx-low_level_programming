acad8615bb6b1e2a537606f84871e6b5b3a0e4fa
Supermjork <markehabaziz@gmail.com>
2023-07-24 17:10:12 +0300

    we goin bacc bois

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reversed string
+ *
+ * @str: string ptr
+ *
+ * Return: void
+ */
+
+void print_rev(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+		len++;
+
+	for (len = len - 1; len >= 0; len--)
+		_putchar(*(str + len));
+
+	_putchar(10);
+}
