275e0aa078cced5dc32a9291f470846736355eff
Supermjork <markehabaziz@gmail.com>
2023-07-24 23:22:15 +0300

    bro be selling neurons

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _atoi - bootleg atoi
+ *
+ * @str: passed string
+ *
+ * Return: int
+ */
+
+int _atoi(char *str)
+{
+	int value, sign, idx;
+
+	/* sign to consider all '-' in the string */
+	sign = 1;
+	/* value to extract the number from the string */
+	value = 0;
+
+	/* iterating over each character to decide the last sign */
+	for (idx = 0; *(str + idx) != '\0'; !(str[idx] > 47 && str[idx] < 58; idx++))
+	{
+		if (*(str + idx) == 45)
+			sign *= -1;
+	}
+
+	for (idx = 0; *(str + idx) != 0; idx++)
+	{
+		if (*(str + idx) > 47 && *(str + idx) < 58)
+			value = (value * 10) + (sign * (*(str + idx) - 48));
+		if (value != 0 && !(*(str + idx) > 47 && *(str + idx) < 58))
+			return (value);
+	}
+	return (value);
+}
