838930ca590a671daeac31ab6b26c17c0e2f8ed7
Supermjork <markehabaziz@gmail.com>
2023-07-24 21:43:38 +0300

    electrricity baccc lesgoo

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in string
+ *
+ * @str: passed string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int idx;
+
+	for (idx = 0; *str != '\n'; str++)
+	{
+		_putchar(*(str + idx));
+		idx += 2;
+	}
+	_putchar(10);
+}
