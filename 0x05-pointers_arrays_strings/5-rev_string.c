7706c7c29f1c2fc23fc89edbcf1269e1cbcc34e7
Supermjork <markehabaziz@gmail.com>
2023-07-24 17:40:10 +0300

    poopystinky

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,15 @@
 void rev_string(char *str)
 {
 	char rev[1000];
-	int r_len, s_len;
+	int r_len = 0;
+	int s_Len = 0;
 
-	for (s_len = 0; *str != '\0'; str++)
+	while (*str != '\0')
+	{
 		s_len++;
+	}
 
-	for (r_len = 0; s_len > 0; s_len--)
+	for (s_len > 0; s_len--)
 	{
 		rev[r_len] = *(str + s_len - 1);
 		r_len++;
