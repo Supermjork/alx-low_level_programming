f8ecb37a4fafc975761646605347373bf27b651c
Supermjork <markehabaziz@gmail.com>
2023-07-27 16:50:33 +0300

    var name swapperino

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,7 @@ void reverse_array(int *array, int n)
 {
 	int ptr, tmp;
 
-	for (ptr = 0; i < n; i++)
+	for (ptr = 0; ptr < n; ptr++)
 	{
 		tmp = *(array + n - 1);
 		*(array + n - 1) = *(array + ptr);
