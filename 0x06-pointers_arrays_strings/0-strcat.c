3791e8a1e746ad5d6da7ce55794febecd2981ce4
Supermjork <markehabaziz@gmail.com>
2023-07-27 14:44:53 +0300

    lappy go brr

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: first half of output str
+ * @src: string to be appended
+ *
+ * Return: char (Appended string)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len_d, len_s;
+
+	for (len_d = 0; *(dest + len_d) != '\0'; dest++)
+	{
+		len_d++;
+	}
+
+	for (len_s = 0; *(src + len_s) != '\0'; len_s++)
+	{
+		*(dest + len_d) = *(src + len_s);
+		len_d++
+	}
+
+	*(dest + len_d) = *(src + len_s);
+
+	return (dest);
+}
