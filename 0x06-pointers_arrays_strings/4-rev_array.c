346d511880a9bef4a9608dc7667be967fc6fe80c
Supermjork <markehabaziz@gmail.com>
2023-07-27 16:48:15 +0300

    pidor

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses passed array
+ *
+ * @array: passed array to be reversed
+ * @n: number of elements to be reversed
+ *
+ * Return: void
+ */
+
+void reverse_array(int *array, int n)
+{
+	int ptr, tmp;
+
+	for (ptr = 0; i < n; i++)
+	{
+		tmp = *(array + n - 1);
+		*(array + n - 1) = *(array + ptr);
+		*(array + ptr) = tmp;
+		n--;
+	}
+}
