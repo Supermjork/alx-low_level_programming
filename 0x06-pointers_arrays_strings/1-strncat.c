b50981ba4c0f1301f02f63145d5a822c1cbca6df
Supermjork <markehabaziz@gmail.com>
2023-07-27 15:59:00 +0300

    lesgoooooooooo

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes into other str
+ *
+ * @dest: str concatenated on
+ * @src: str to concat from
+ * @n: n-bytes from str
+ *
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_d, len_s;
+
+	len_d = 0;
+
+	while (*(dest + len_d) != '\0')
+	{
+		len_d++;
+	}
+
+	for (len_s = 0; len_s < n; len_s++)
+	{
+		*(dest + len_d) = *(src + len_s);
+		len_d++;
+	}
+
+	if (*(dest + len_d - 1) != '\0')
+	{
+		*(dest + len_d) = '\0';
+	}
+
+	return (dest);
+}
