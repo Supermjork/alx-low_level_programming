dbb3d28cd6d7d0d0da966aa75d3d575079285fd7
Supermjork <markehabaziz@gmail.com>
2023-07-27 16:29:01 +0300

    wokrkkkkrkkr

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Bool
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int ptr;
+
+	ptr = 0;
+
+	while (s1[ptr] != '\0' && s2 != '\0')
+	{
+		if (s1[ptr] != s2[ptr])
+			return (s1[ptr] - s2[ptr]);
+		ptr++;
+	}
+
+	if (s1[ptr] == s2[ptr])
+		return (0);
+	return (s1[ptr] = s2[ptr]);
+}
