241f39a5c9c9f77490f1a45a51cf87a82bf2c075
Supermjork <markehabaziz@gmail.com>
2023-07-31 13:16:49 +0300

    starting off

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - fills first n bytes of memory with constant byte
+ *
+ * @s: memory area pointer
+ * @b: constant byte, filler
+ * @n: number of bytes to fill within s
+ *
+ * Return: pointer to area (s)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# POWER OUTAGES STOPPED
+LESGOOOOOOOOOOOOOOOO
+well, until further notice at least smh
