93e79acbaf5057b91886051dc2a2aef7965ab567
Supermjork <markehabaziz@gmail.com>
2023-07-31 16:39:40 +0300

    EZ FIX

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,12 +15,13 @@ void print_diagsums(int *mat, int size)
 
 	for (idx = 0; idx < size; idx++)
 	{
-		main_diag += *(*(mat + idx) + idx);
+		/* Multiplying size by index, jumps rows */
+		main_diag += mat[idx * size + idx];
 	}
 
 	for (idx = 0; idx <= size; idx++)
 	{
-		sec_diag += *(*(mat + idx) + size - idx);
+		sec_diag += mat[idx * size + (size - idx)];
 	}
 
 	printf("%d, %d\n", main_diag, sec_diag);
