4c4c578bdc2733c1fdf02ddeefeab144ab236219
Supermjork <markehabaziz@gmail.com>
2023-08-02 13:35:10 +0300

    14

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of string
+ *
+ * @str: string passed
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *str)
+{
+	if (*str == '\0')
+		return 0;
+
+	if (*(str + 1) == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (_str_len_recursion(str + 1) + 1);
+	}
+}
