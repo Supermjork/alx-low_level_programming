81a1cf38c1e9387b9989bb01bd4d8abd46d8d403
Supermjork <markehabaziz@gmail.com>
2023-08-04 21:18:27 +0300

    rapido

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - print number of args
+ *
+ * @argc: number of args
+ * @argv: args passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	argv = argv;
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - pain of multiplication
+ *
+ * @argc: number of args
+ * @argv: args
+ *
+ * Return: 1 on error, 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num0, num1;
+	char *n0 = argv[1];
+	char *n1 = argv[2];
+
+	num0 = 0;
+	num1 = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (n0 == '-')
+		num0 = -1 * atoi(&n0[1]);
+	else
+		num0 = atoi(&n0[0]);
+
+	if (n1 == '-')
+		num1 = -1 * atoi(&n1[1]);
+	else
+		num1 = atoi(&n1[0]);
+
+	printf("%d\n", num0 * num1);
+
+	return (0);
+}
