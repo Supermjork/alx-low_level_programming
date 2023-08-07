0cfd23235d8c3ff4d6afdd74a4b3bf5fdab5aaf7
Supermjork <markehabaziz@gmail.com>
2023-08-07 18:32:13 +0300

    Foot shooting rootin tootin

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - Allocates an contiguos area of memory and fills with a char
+ *
+ * @size: size of array
+ * @c: character to fill with
+ *
+ * Return: pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr_head;
+	unsigned int idx;
+
+	if (size == 0)
+		return (NULL);
+
+	arr_head = (char *)malloc(size * sizeof(char));
+
+	if (arr_head == NULL)
+		return (NULL);
+
+	for (idx = 0; idx < size; idx++)
+		*(arr_head + idx) = c;
+
+	return (arr_head);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# Foot shooting galore
