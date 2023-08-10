ac8e366932a79fc5c681be9883f60f6af10dc2ec
Supermjork <markehabaziz@gmail.com>
2023-08-10 15:06:30 +0300

    istg we've done concatenation too many times pls

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * _strlen - length of string
+ *
+ * @s: string passed
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	if (s == NULL)
+		return (1);
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		;
+
+	return (i + 1);
+}
+
+/**
+ * string_nconcat - concatenate n characters
+ *
+ * @str0: first string passed
+ * @str1: second string passed
+ * @n: n bytes
+ *
+ * Return: ptr to new concatenated string
+ */
+
+char *string_nconcat(char *str0, char *str1, unsigned int n)
+{
+	char *finalstr;
+
+	unsigned int size0, size1, idx0, idx1;
+
+	size0 = _strlen(str0);
+	size1 = _strlen(str1);
+
+	if (n > size1 - 1)
+		n = size1 - 1;
+
+	finalstr = (char *)malloc((size0 + n) * sizeof(char));
+
+	if (finalstr == NULL)
+		return (NULL);
+	if (size0 == 1 && size1 == 1)
+		return (NULL);
+
+	for (idx0 = 0; idx0 < size0 - 1; idx0++)
+	{
+		*(finalstr + idx0) = *(str0 + idx0);
+	}
+
+	for (idx1 = 0; idx1 <= n; idx1++)
+	{
+		*(finalstr + idx0 + idx1) = *(str1 + idx1);
+	}
+
+	*(finalstr + idx0 + idx1) = '\0';
+
+	return (finalstr);
+}
