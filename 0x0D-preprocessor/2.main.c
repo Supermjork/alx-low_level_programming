323349f017119d238622c89e69122c00d8ed6c2f
Supermjork <markehabaziz@gmail.com>
2023-08-14 17:35:12 +0300

    brrrrrrr

---
+++ 0x0D-preprocessor/2.main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - prints name of file compiled from
+ *
+ * Return: 0 (On success) (ALWAYS)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
